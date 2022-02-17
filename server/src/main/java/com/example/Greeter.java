c378b6edee27758b288fed19fa8723f928ab8c4e
akshata <akshataraikar08@gmail.com>
2022-02-16 21:10:24 -0800

    changes by demo

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,4 +16,11 @@ public class Greeter {
   public String greet(String someone) {
     return String.format("Hello How are you hope all are good, Yes Evn I am good . fine thank you , %s!", someone);
   }
+  
+  public String greet11(String someone) {
+	    return String.format("what is going on , %s!", someone);
+	  }
+  public String greet12(String someone) {
+	    return String.format("I am doing good , %s!", someone);
+	  }
 }
